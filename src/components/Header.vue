f08aa6f5192b5cecf93039652524dbca32f6ae05
wurc <wurc98@qq.com>
2022-04-25 11:36:10 +0800

    init

---
+++ src/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="header">
+    滚动轴承故障诊断记录
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
